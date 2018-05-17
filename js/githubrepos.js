63febe4a5029c9be822ad17c06eea3222bf1f2a9
charan <charan.r@zysk.in>
2018-05-17 15:58:54 +0530

    Fixed font size in homepage

---
+++ js/githubrepos.js
@@ -0,0 +1,48 @@
+function getGithubRepo(count , sliderBool) {
+    var gojeckAPI = "https://api.github.com/orgs/gojek-engineering/repos";
+    $.getJSON(gojeckAPI,
+        {
+            format: "json"
+        })
+        .done(function (data) {
+            var i,src;
+            count === 10 ? src = '../img/sample-img-2.png' : src = 'img/sample-img-2.png';
+            // if(count === 10){
+            //     src = '../img/sample-img-2.png' ;
+            // }
+            // else{
+            //     src = 'img/sample-img-2.png' ;
+            // }
+            
+            for (i = 0; i < count; i++) {
+                $("#opensourceCards").append(
+                    '<div class="col-md-5 rounded">' +
+                    '<div class="card border-0 mb-4 flex-row opensource-card">' +
+                    '<img class="align-self-center p-3 " src="'+ src +'" alt="Sample repository">' +
+                    '<div class="card-body">' +
+                    '<h3 class="mb-0 card-title">' +
+                    '<a class="text-dark card-name" title = "' + data[i].name + '" href="https://github.com/gojek-engineering/go-ratelimit" target="_blank">' + data[i].name + '</a>' +
+                    '</h3>' +
+                    '<p class="description card-text mb-auto">' + (data[i].description !== null ? data[i].description : "") + '</p>' +
+                    ' </div>' +
+                    ' </div>' +
+                    '</div>'
+                );
+            }
+          
+            if(sliderBool){
+                $('#opensourceCards').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    mobileFirst: true,
+                    responsive: [
+                        {
+                            breakpoint: 767,
+                            settings: 'unslick'
+                        }
+                    ]
+                });
+            }
+
+        });
+}
